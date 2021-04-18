d8c28c4167d27a275882dddaca83b8103047c419
Q3behet <marcelina.baranowska@protonmail.com>
2021-04-18 16:57:44 +0200

    1.2 pd1.1

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response, status
 
 app = FastAPI()
+app.counter = 0
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello world!"}
+
+@app.get("/counter")
+def counter():
+    app.counter = app.counter + 1
+    return app.counter
---
+++ tests.py
@@ -0,0 +1,18 @@
+import pytest
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
+
+def test_counter():
+    response = client.get("/counter")
+    assert response.status_code == 200
+    assert response.text == "1"
+
+    response = client.get("/counter")
+    assert response.status_code == 200
+    assert response.text == "2"
