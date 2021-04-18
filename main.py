f6e1a34c5ea8ec70758b98b7d5a927d0ca01746c
Q3behet <marcelina.baranowska@protonmail.com>
2021-04-18 17:22:47 +0200

    1.3 pd1.2

--- main.py
+++ main.py
@@ -3,11 +3,36 @@ from fastapi import FastAPI, Request, Response, status
 app = FastAPI()
 app.counter = 0
 
-@app.get("/")
-def root():
-    return {"message": "Hello world!"}
 
 @app.get("/counter")
 def counter():
     app.counter = app.counter + 1
     return app.counter
+
+
+# 1.1
+@app.get("/")
+def root():
+    return {"message": "Hello world!"}
+
+
+# 1.2
+@app.get("/method")
+def method_get():
+    return {"method": "GET"}
+
+@app.post("/method", status_code=201)
+def method_post():
+    return {"method": "POST"}
+
+@app.delete("/method")
+def method_delete():
+    return {"method": "DELETE"}
+
+@app.put("/method")
+def method_put():
+    return {"method": "PUT"}
+
+@app.options("/method")
+def method_options():
+    return {"method": "OPTIONS"}
--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+    assert response.json() == {"message": "Hello world!"}
 
 def test_counter():
     response = client.get("/counter")
@@ -16,3 +16,8 @@ def test_counter():
     response = client.get("/counter")
     assert response.status_code == 200
     assert response.text == "2"
+
+def test_method_post():
+    response = client.post("/method")
+    assert response.status_code == 201
+    assert response.json() == {"method": "POST"}
