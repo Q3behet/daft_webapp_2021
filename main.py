d58cf43e1f4c5f73477b49fd50d7fe6f01c3b4ba
Q3behet <marcelina.baranowska@protonmail.com>
2021-04-18 18:04:40 +0200

    1.4 pd1.3

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Request, Response, status
+from fastapi import FastAPI, Request, Response, status, HTTPException
+from typing import Optional
+import hashlib
 
 app = FastAPI()
 app.counter = 0
@@ -21,18 +23,29 @@ def root():
 def method_get():
     return {"method": "GET"}
 
+
 @app.post("/method", status_code=201)
 def method_post():
     return {"method": "POST"}
+
 
 @app.delete("/method")
 def method_delete():
     return {"method": "DELETE"}
 
+
 @app.put("/method")
 def method_put():
     return {"method": "PUT"}
 
+
 @app.options("/method")
 def method_options():
     return {"method": "OPTIONS"}
+
+
+# 1.3
+@app.get("/auth", status_code=204)
+def check_password(password: Optional[str] = None, password_hash: Optional[str] = None):
+    if password_hash != hashlib.sha512(str(password).encode("utf-8")).hexdigest() or not password or not password_hash:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
